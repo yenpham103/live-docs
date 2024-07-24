37ba4ae94d4236cc914964e97d3a832f6430a7ef
phamyen1003 <nhovequehuong1003@gmail.com>
2024-07-24 23:40:12 +0700

    feat: setup liveblocks

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -18,19 +18,17 @@ declare global {
     UserMeta: {
       id: string;
       info: {
-        id: string;
-        name: string;
-        email: string;
-        avatar: string;
-        color: string;
+        // Example properties, for useSelf, useUser, useOthers, etc.
+        // name: string;
+        // avatar: string;
       };
     };
 
     // Custom events, for useBroadcastEvent, useEventListener
     RoomEvent: {};
-    // Example has two events, using a union
-    // | { type: "PLAY" }
-    // | { type: "REACTION"; emoji: "🔥" };
+      // Example has two events, using a union
+      // | { type: "PLAY" } 
+      // | { type: "REACTION"; emoji: "🔥" };
 
     // Custom metadata set on threads, for useThreads, useCreateThread, etc.
     ThreadMetadata: {
